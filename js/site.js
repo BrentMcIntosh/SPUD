7b6987bbdd466325fbef15c20d9470ca7f9b55c1
Brent McIntosh <brent.mcintosh@alight.com>
2021-02-21 19:04:55 -0500

    Genres Complete

--- js/site.js
+++ js/site.js
@@ -27,11 +27,7 @@ function replace(page) {
 
                         var item = list[i];
 
-                        var row = template;
-
-                        row = row.replace(/{{genreId}}/g, item.genreId);
-                        row = row.replace(/{{name}}/g, item.name);
-                        row = row.replace(/{{description}}/g, item.description);
+                        var row = interpolate(template, item.genreId, item.name, item.description);
 
                         if (i === 0) {
                             tbody.getElementsByTagName('tr')[0].innerHTML = row;
@@ -57,11 +53,9 @@ function replace(page) {
     xhttp.send();
 }
 
-
 function update(list) {
     simplePage('Update', list);
 }
-
 
 function remove(list) {
     simplePage('Delete', list);
@@ -73,19 +67,62 @@ function simplePage(page, list) {
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
 
-            var html = this.responseText;
-
             var items = list.split('|');
 
-            html = html.replace(/{{genreId}}/g, items[0]);
-            html = html.replace(/{{name}}/g, items[1]);
-            html = html.replace(/{{description}}/g, items[2]);
-
-            document.getElementById('main').innerHTML = html;
+            document.getElementById('main').innerHTML = interpolate(this.responseText, items[0], items[1], items[2]);
         }
     };
 
     xhttp.open('GET', '/Views/Genres/' + page + '.html', true);
     xhttp.send();
+}
 
+function interpolate(template, genreId, name, description) {
+    template = template.replace(/{{genreId}}/g, genreId);
+    template = template.replace(/{{name}}/g, name);
+    template = template.replace(/{{description}}/g, description);
+
+    return template;
+}
+
+function save(genreId) {
+
+    var xhttp = new XMLHttpRequest();
+
+    var data = '{ "genreId": {{genreId}}, "name": "{{name}}", "description": "{{description}}" }';
+
+    var name = document.getElementById("name").value;
+    var description = document.getElementById("description").value;
+
+    data = interpolate(data, genreId, name, description);
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            replace('Genres');
+        }
+    };
+
+    if (genreId === 0) {
+        xhttp.open('POST', '/api/Genres', true);
+    }
+    else {
+        xhttp.open('PUT', '/api/Genres/' + genreId, true);
+    }
+
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(data);
+}
+
+function reallyDelete(genreId) {
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            replace('Genres');
+        }
+    };
+
+    xhttp.open('DELETE', '/api/Genres/' + genreId, true);
+    xhttp.send();
 }
